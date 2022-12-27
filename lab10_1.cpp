12998af764439dc9f9b9ac41da585288b5831b39
Lolorpof <sirawit_sir@cmu.ac.th>
2022-12-27 15:56:19 +0700

    lab10_1 completed

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -3,9 +3,13 @@
 using namespace std;
 
 int main(){	
+	double loan, interest, pay;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> interest;
 	cout << "Enter amount you can pay per year: ";
+	cin >> pay;
 
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
@@ -21,13 +25,51 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	int i = 1;
+	double prev_bal = loan, new_bal, t_interest, total;
+
+	t_interest = (interest/100) * prev_bal;
+	total = prev_bal + t_interest;
+	if(pay > total){
+		new_bal = total - total;
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << prev_bal;
+		cout << setw(13) << left << t_interest;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << total;
+		cout << setw(13) << left << new_bal;
+	}
+
+	else{
+		while(new_bal != 0){
+
+			t_interest = (interest/100) * prev_bal;
+			total = prev_bal + t_interest;
+			new_bal = total - pay;
+
+			if(total >= pay){
+				cout << setw(13) << left << i; 
+				cout << setw(13) << left << prev_bal;
+				cout << setw(13) << left << t_interest;
+				cout << setw(13) << left << total;
+				cout << setw(13) << left << pay;
+				cout << setw(13) << left << new_bal;
+			}
+			else{
+				new_bal = 0;
+				cout << setw(13) << left << i; 
+				cout << setw(13) << left << prev_bal;
+				cout << setw(13) << left << t_interest;
+				cout << setw(13) << left << total;
+				cout << setw(13) << left << total;
+				cout << setw(13) << left << new_bal;
+			}
+		prev_bal = new_bal;
+
+		i++;
+		cout << "\n";	
+		}
+	}
 	
 	return 0;
 }
